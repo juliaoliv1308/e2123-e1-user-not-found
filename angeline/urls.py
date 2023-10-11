c3fc6087a7e275cb921d608da26416449df531bc
Seu Nome <seu_email@example.com>
2023-10-11 16:45:38 -0300

    Primeiro teste funcionado sem erro

--- angeline/urls.py
+++ angeline/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import helloworld
 
-app_name = 'main'
+app_name = 'angeline'
 
 urlpatterns = [
     path('', helloworld, name='helloworld'),
--- angeline/views.py
+++ angeline/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def helloworld(request):
+    pass
     return render(request, 'angeline/index.html')
 
  
