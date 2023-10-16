84a92084eefcef7a1d497a5fadc2baccffdb1761
Victor Gabriel <victorgabrieljubileu@gmail.com>
2023-10-16 16:01:28 -0300

    ok

--- angeline/dicionarios.py
+++ angeline/dicionarios.py
@@ -25,13 +25,13 @@ suspense = {
 }
 
 romance = {
-    "1": {"nome":"","resumo":"","linkimg":"","pdf":""},
-    "2": {"nome":"","resumo":"","linkimg":"","pdf":""},
-    "3": {"nome":"","resumo":"","linkimg":"","pdf":""},
-    "4": {"nome":"","resumo":"","linkimg":"","pdf":""},
-    "5": {"nome":"","resumo":"","linkimg":"","pdf":""},
-    "6": {"nome":"","resumo":"","linkimg":"","pdf":""},
-    "7": {"nome":"","resumo":"","linkimg":"","pdf":""},
+    "1": {"nome":"ahipotesedoamor","resumo":"","linkimg":"","pdf":""},
+    "2": {"nome":"avidainvisiveldeaddielarue","resumo":"","linkimg":"","pdf":""},
+    "3": {"nome":"Daisy Jones and the Six","resumo":"","linkimg":"","pdf":""},
+    "4": {"nome":"eassimqueacaba","resumo":"","linkimg":"","pdf":""},
+    "5": {"nome":"eassimquecomeca","resumo":"","linkimg":"","pdf":""},
+    "6": {"nome":"umlugarbemlongedaqui","resumo":"","linkimg":"","pdf":""},
+    "7": {"nome":"alice","resumo":"","linkimg":"","pdf":""},
     "8": {"nome":"","resumo":"","linkimg":"","pdf":""},
     "9": {"nome":"","resumo":"","linkimg":"","pdf":""},
     "10": {"nome":"","resumo":"","linkimg":"","pdf":""},
