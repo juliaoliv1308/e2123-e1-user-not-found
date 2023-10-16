216899dafb40158048e2254b82f801dfbc3f6628
Seu Nome <seu_email@example.com>
2023-10-16 15:45:09 -0300

    Criando a separação das litas para a pagina principal

--- angeline/views.py
+++ angeline/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def helloworld(request):
-    pass
+    for id_livro, info in principal.items():
+        id_fornecedor = info['codigo_fornecedor']
+        fornecedor_info = fornecedores.get(id_fornecedor, "ID não encontrado")
+
+        
+        if fornecedor_info and categoria_info != "ID não encontrado":
+            lista_fusão[id_livro] = {
+                'nome': info['nome'],
+            }
+            media = media + info['diasvencidos']
+        else:
+            lista_fusão[id_livro] = {
+                'nome': info['nome'],
+            }
+
+    context = {
+
+    }
     return render(request, 'angeline/index.html')
 
  
