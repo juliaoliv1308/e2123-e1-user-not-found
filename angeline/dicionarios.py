3084b4c6d395a1d4bc52736f10e08b57a6458da2
Victor Gabriel <victorgabrieljubileu@gmail.com>
2023-10-18 16:19:45 -0300

    ok

--- angeline/dicionarios.py
+++ angeline/dicionarios.py
@@ -37,27 +37,27 @@ suspense = {
 romance = {
     1: {
         "nome":"É Assim Que Acaba",
-        "resumo":"É Assim que Acaba de Colleen Hoover é uma história sobre trauma, recuperação e amor, centrando-se em Lily Bloom e sua jornada para superar eventos traumáticos e encontrar a cura emocional através do apoio de um amor do passado.",
+        "resumo":"É Assim que Acaba de Colleen Hoover acompanha Lily Bloom enquanto ela busca superar traumas e encontrar cura emocional através do apoio de um amor do passado.",
         "linkimg":"main/livros/romance/eassimqueacaba.jpg",
         "pdf":"",
         "nota": 6.5
         },
     2: {
         "nome":"A Razão do Amor",
-        "resumo":"A Razão do Amor é um livro de Kristin Hannah que narra a jornada de duas irmãs, relembrando sua complicada relação enquanto enfrentam desafios pessoais e encontram a verdadeira razão do amor familiar. A história explora temas de perdão, aceitação e laços familiares.",
+        "resumo":"A Razão do Amor de Kristin Hannah explora a complicada relação entre duas irmãs enquanto enfrentam desafios pessoais e descobrem o verdadeiro significado do amor familiar, destacando temas de perdão e aceitação.",
         "linkimg":"main/livros/romance/arazaodoamor.jpg",
         "pdf":"",
         "nota": 4.1
         },
     3: {
         "nome":"As Mil Partes do Meu Coração.",
-        "resumo":"As Mil Partes do Meu Coração é um livro de Colleen Hoover que conta a história de Merit Voss, uma jovem com uma família disfuncional e segredos sombrios. O livro explora temas de amor, perdão e aceitação enquanto Merit lida com os segredos de sua família e encontra seu próprio caminho.",
+        "resumo":"As Mil Partes do Meu Coração de Colleen Hoover narra a vida de Merit Voss, uma jovem em uma família disfuncional, explorando temas de amor, perdão e aceitação em meio a segredos familiares sombrios.",
         "linkimg":"main/livros/romance/asmilpartesdomeucoracao.jpg",
         "pdf":"",
         "nota": 7.8
         },
     4: {"nome":"A Vida Invisivel de Addie Larue",
-        "resumo":"A Vida Invisível de Addie Larue de V.E. Schwab narra a vida de Addie, que fez um pacto com um deus obscuro para ser imortal, mas é amaldiçoada a ser esquecida por todos que a encontram. A história explora sua jornada ao longo dos séculos e sua busca por significado e amor.",
+        "resumo":"A Vida Invisível de Addie Larue de V.E. Schwab segue a vida de Addie, amaldiçoada a ser esquecida por todos, enquanto busca significado e amor ao longo dos séculos.",
         "linkimg":"main/livros/romance/avidainvisiveldeaddielarue.jpg",
         "pdf":"",
         "nota": 3.2
@@ -71,14 +71,14 @@ romance = {
         },
     6: {
         "nome":"Como Eu Era Antes de Você",
-        "resumo":"Como Eu Era Antes de Você de Jojo Moyes é uma história de amor entre Louisa Clark e Will Traynor. Louisa é contratada como cuidadora de Will, um homem paralisado, e o livro aborda a transformação de suas vidas enquanto enfrentam desafios e desenvolvem um vínculo profundo.",
+        "resumo":"Louisa é contratada como cuidadora de Will, um homem paralisado, e o livro aborda a transformação de suas vidas enquanto enfrentam desafios e desenvolvem um vínculo profundo.",
         "linkimg":"main/livros/romance/comoeueraantesdevoce.jpg",
         "pdf":"",
         "nota": 9.9
         },
     7: {
         "nome":"Daisy Jones & The Six",
-        "resumo":"Daisy Jones & The Six de Taylor Jenkins Reid é um livro que narra a história fictícia de uma banda de rock dos anos 1970, explorando seus altos e baixos, relações pessoais e a criação de músicas icônicas. É escrito no formato de uma entrevista oral, oferecendo uma visão única da indústria musical e das vidas dos personagens.",
+        "resumo":"Daisy Jones & The Six de Taylor Jenkins Reid é uma narrativa fictícia sobre uma banda de rock dos anos 1970, contada por meio de entrevistas orais, explorando sua jornada, relacionamentos e criação de músicas icônicas.",
         "linkimg":"main/livros/romance/daisyjonesandthesix.jpg",
         "pdf":"",
         "nota": 8.2
@@ -99,7 +99,7 @@ romance = {
         },
     10: {
         "nome":"Um Lugar Bem Longe Daqui",
-        "resumo":"Um Lugar Bem Longe Daqui de Delia Owens é uma história que segue a vida de Kya Clark, a 'menina do pântano', enquanto ela cresce isolada na Carolina do Norte. A trama explora solidão, natureza e mistério, com a descoberta do corpo de um homem morto e a investigação subsequente que revela segredos do passado.",
+        "resumo":"Um Lugar Bem Longe Daqui de Delia Owens narra a vida solitária de Kya Clark no pântano da Carolina do Norte, incluindo a descoberta de um corpo e uma investigação que desvenda segredos do passado.",
         "linkimg":"main/livros/romance/umlugarbemlongedaqui.jpg",
         "pdf":"",
         "nota": 4.8
@@ -108,21 +108,21 @@ romance = {
 biografias = {
     1: {
         "nome":"A Marca da Vitória",
-        "resumo":"A Marca da Vitória é um livro de Phil Knight que narra a história da Nike, desde sua fundação como Blue Ribbon Sports até se tornar uma gigante mundial do mercado esportivo. O livro destaca a perseverança e a paixão de Knight pelo negócio e como ele construiu uma marca icônica.",
+        "resumo":"O livro destaca a perseverança e a paixão de Knight pelo negócio e como ele construiu uma marca icônica.",
         "linkimg":"main/livros/biografias/amarcadavitoria.jpeg",
         "pdf":"",
         "nota": 10
         },
     2: {
         "nome":"Anne Frank",
-        "resumo":"O Diário de Anne Frank é um relato escrito por Anne Frank, uma adolescente judia, durante a Segunda Guerra Mundial, enquanto ela e sua família se escondiam dos nazistas em Amsterdã. O livro documenta seus pensamentos, medos e experiências, oferecendo um testemunho impactante do Holocausto.",
+        "resumo":"O Diário de Anne Frank é o relato comovente de Anne Frank, uma adolescente judia durante a Segunda Guerra Mundial, que descreve sua vida escondida dos nazistas em Amsterdã, oferecendo um testemunho impactante do Holocausto.",
         "linkimg":"main/livros/biografias/annefrank.jpeg",
         "pdf":"",
         "nota": 6.4
     },
     3: {
         "nome":"Elon Musk",
-        "resumo":"O livro Elon Musk: Tesla, SpaceX e a Busca por um Futuro Fantástico de Ashlee Vance retrata a vida e carreira de Elon Musk, destacando sua visão empreendedora e sua busca para revolucionar a indústria espacial, de veículos elétricos e solar, revelando seus desafios e sucessos notáveis.",
+        "resumo":"Elon Musk Tesla, SpaceX e a Busca por um Futuro Fantástico de Ashlee Vance narra a vida e a busca empreendedora de Elon Musk para revolucionar indústrias como a espacial, de veículos elétricos e solar, destacando seus desafios e sucessos notáveis.",
         "linkimg":"main/livros/biografias/elonmusk.jpeg",
         "pdf":"",
         "nota": 2.3
@@ -136,28 +136,28 @@ biografias = {
         },
     5: {
         "nome":"Eu Sou Malala"
-        ,"resumo":"Eu Sou Malala é a autobiografia de Malala Yousafzai, uma ativista paquistanesa que sobreviveu a um atentado do Talibã por lutar pelo direito das meninas à educação. O livro conta sua jornada corajosa e inspiradora em defesa da educação e dos direitos das mulheres.",
+        ,"resumo":"Eu Sou Malala narra a corajosa jornada de Malala Yousafzai, uma ativista paquistanesa que sobreviveu a um ataque do Talibã e se tornou uma defensora dos direitos das mulheres e da educação.",
         "linkimg":"main/livros/biografias/eusoumalala.jpeg",
         "pdf":"",
         "nota": 3.7
         },
     6: {
         "nome":"Leonardo Da Vinci",
-        "resumo":"O livro Leonardo da Vinci de Walter Isaacson é uma biografia que explora a vida e obra do renomado gênio renascentista, revelando seus notáveis talentos como pintor, inventor, cientista e inovador, enquanto oferece uma visão íntima de sua complexa personalidade.",
+        "resumo":"Leonardo da Vinci de Walter Isaacson é uma biografia que revela a vida e múltiplos talentos do gênio renascentista, explorando suas realizações como pintor, inventor e cientista, além de sua complexa personalidade.",
         "linkimg":"main/livros/biografias/leonardodavinci.jpeg",
         "pdf":"",
         "nota": 9.0
         },
     7: {
         "nome":"Minha História",
-        "resumo":"O livro Minha História de Michelle Obama é uma autobiografia que narra a vida da ex-primeira-dama dos EUA, desde sua infância em Chicago até seus anos na Casa Branca. O livro aborda questões de identidade, igualdade e suas experiências como mulher e mãe em uma posição de influência.",
+        "resumo":"Minha História de Michelle Obama é uma autobiografia que explora sua jornada desde a infância em Chicago até os anos na Casa Branca, abordando temas de identidade, igualdade e suas experiências como mulher e mãe em uma posição de influência.",
         "linkimg":"main/livros/biografias/michelleobama.jpeg",
         "pdf":"",
         "nota": 1.8
         },
     8: {
         "nome":"Príncipe Harry",
-        "resumo":"O que sobra é uma autobiografia do príncipe, onde ele conta sua história de vida desde sua infância até sua decisão de se afastar da realeza britânica com sua esposa, Meghan Markle. O livro começa com o Príncipe Harry descrevendo sua infância a partir do momento da morte de sua mãe, a princesa Diana.",
+        "resumo":"O que sobra é uma autobiografia do Príncipe Harry que começa com sua infância após a morte de sua mãe, a Princesa Diana, e aborda sua vida, decisão de se afastar da realeza britânica e relacionamento com Meghan Markle.",
         "linkimg":"main/livros/biografias/principeharry.jpeg",
         "pdf":"",
         "nota": 7.5
