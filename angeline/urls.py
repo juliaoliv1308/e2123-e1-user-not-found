9592b9cb665d518747108e6e9ca67e9ea84e25f6
Seu Nome <seu_email@example.com>
2023-10-18 16:10:33 -0300

    Barra de pesquisa funcional

--- angeline/urls.py
+++ angeline/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import helloworld
+from .views import helloworld, pesquisar_livros
 
 app_name = 'angeline'
 
 urlpatterns = [
     path('', helloworld, name='helloworld'),
+    path('pesquisar/', pesquisar_livros, name='pesquisar_livros'),
 ]
