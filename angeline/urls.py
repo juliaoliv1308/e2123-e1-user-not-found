ce00a977db035d865581c3e543e599a5dfa54478
Victor Gabriel <victorgabrieljubileu@gmail.com>
2023-10-19 13:29:22 -0300

    Atualização diaria

--- angeline/urls.py
+++ angeline/urls.py
@@ -1,18 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
-from .views import helloworld, books
-=======
-from .views import helloworld, pesquisar_livros
->>>>>>> 4828a441a843750052ecbade9992b9c86b078408
+from .views import helloworld, books, pesquisar_livros
 
 app_name = 'angeline'
 
 urlpatterns = [
     path('', helloworld, name='helloworld'),
-<<<<<<< HEAD
-    path('books', books, name='books')
-=======
+    path('books', books, name='books'),
     path('pesquisar/', pesquisar_livros, name='pesquisar_livros'),
->>>>>>> 4828a441a843750052ecbade9992b9c86b078408
 ]
