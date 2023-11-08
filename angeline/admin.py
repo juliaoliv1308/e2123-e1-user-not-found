eab2c0cbd28c3d4e5a57beca66c1be8ca2b7de4b
Adriano Machado <adriano@machado.tec.br>
2023-11-08 14:23:13 -0300

    ok by prof

--- angeline/admin.py
+++ angeline/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Livro
+from .models import Livro, Autor
 
 # Register your models here.
 
 admin.site.register(Livro)
+admin.site.register(Autor)
--- angeline/models.py
+++ angeline/models.py
@@ -6,10 +6,18 @@ from django.db import models
 
 class Autor(models.Model):
     id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=255, verbose_name='Author name', null=True)
+    nome = models.CharField(max_length=255, verbose_name='Author name', null=True, blank=True)
 
     def __str__(self):
-        return self.nome
+        # Retorna o nome do autor e o ID em um formato string.
+        return f"{self.nome} {self.id}"
+
+    class Meta:
+        # Define a ordenação padrão para ser pelo nome.
+        ordering = ['nome']
+
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
 
 class Editora(models.Model):
     id = models.AutoField(primary_key=True)
