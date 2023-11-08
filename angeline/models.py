151b58d55cdbcacd107487f6c03c0bfcbc7d7207
Seu Nome <seu_email@example.com>
2023-11-08 15:56:52 -0300

    ok

--- angeline/models.py
+++ angeline/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Autor(models.Model):
     id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=255, verbose_name='Author Name', null=True, blank=True)
+    nome = models.CharField(max_length=255, verbose_name='Nome Autor', null=True, blank=True)
 
     def __str__(self):
         # Retorna o nome do autor e o ID em um formato string.
@@ -21,8 +21,8 @@ class Autor(models.Model):
 
 class Editora(models.Model):
     id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=255, verbose_name='Publisher name', null=True)
-    livro_nome = models.CharField(max_length=255, verbose_name='Book title', null=True)
+    nome = models.CharField(max_length=255, verbose_name='Nome Editora', null=True)
+    livro_nome = models.CharField(max_length=255, verbose_name='Titulo Livro', null=True)
 
     def __str__(self):
         # Retorna o nome do autor e o ID em um formato string.
@@ -60,8 +60,9 @@ class Livro(models.Model):
     sub_categoria = models.CharField(max_length=255, blank=True)  # Campo em branco, se necessário
     estoque = models.IntegerField(null=True)
     categoria = models.IntegerField(null=True)
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, verbose_name='Author', null=True)
-    editora = models.ForeignKey(Editora, on_delete=models.CASCADE, verbose_name='Publisher', null=True)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, verbose_name='Autor', null=True)
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE, verbose_name='Editora', null=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, verbose_name='Status', null=True)
 
     def __str__(self):
         return f'{self.nome} - {self.id}'
