4cff9fcfcc53f65b6eb4205848a38974083d8309
Seu Nome <seu_email@example.com>
2023-11-09 15:56:30 -0300

    admin e models

--- angeline/admin.py
+++ angeline/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import Livro, Editora, Autor, Status
-
+from .models import Livro, Editora, Autor, Status, Avaliacao_usuario
 # Register your models here.
 
-admin.site.register(Livro)
 admin.site.register(Autor)
 admin.site.register(Status)
 admin.site.register(Editora)
+admin.site.register(Avaliacao_usuario)
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'nota_media_display')
+    readonly_fields = ('nota_media_display',)
+
+    def nota_media_display(self, obj):
+        return obj.nota_media
+    nota_media_display.short_description = 'Nota Média'
